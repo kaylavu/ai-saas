a50d4a2bce5c28233b4f512b03c35831a559ac6f
Kayla Vu <kaylaavu@gmail.com>
2024-03-17 13:17:35 -0700

    code generation wip

---
+++ app/api/code/route.ts
@@ -0,0 +1,47 @@
+import { auth } from "@clerk/nextjs";
+import OpenAI from "openai";
+import { NextResponse } from "next/server";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { messages } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    // const instructionMessage = {
+    //   role: "system",
+    //   content:
+    //     "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations.",
+    // };
+    // const params: OpenAI.Chat.ChatCompletionCreateParams = {
+    //   messages: [instructionMessage, ...messages],
+    //   model: "gpt-3.5-turbo",
+    // };
+    // const chatCompletion: OpenAI.Chat.ChatCompletion =
+    //   await openai.chat.completions.create(params);
+    // return NextResponse.json(chatCompletion.choices[0].message);
+
+    // For development purposes, stubbing response to save on API Requests costs.
+    const chatCompletion = {
+      choices: [
+        {
+          message: {
+            role: "assistant",
+            content: "```markdown\n<!-- Hi there! -->\n```",
+          },
+        },
+      ],
+    };
+    return NextResponse.json(chatCompletion.choices[0].message);
+  } catch (error) {
+    console.log("[CODE_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ import { Menu } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <div className="flex items-center p4">
+    <div className="flex items-center p-4">
       <MobileSidebar />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />
